9e3be0a61208eeb9f1926dfcecb656d24c67bdae
putri436 <putriperwita63@gmail.com>
2024-10-31 10:58:18 +0700

    Add files via upload

---
+++ quizPutriTugasKeduaa.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+double total(double bApel, double bJeruk, double bSemangka) {
+	double apel = 24000;
+	double jeruk = 32500;
+	double semangka = 8000;
+	double totalBerat = bApel + bJeruk + bSemangka;
+	double hasil;
+
+	double total = (apel * bApel) + (jeruk * bJeruk) + (semangka * bSemangka);
+
+
+	if (totalBerat >= 10) {
+		hasil = (5.0 / 100) * total;
+	}
+	else if (totalBerat >= 8) {
+		hasil = (2.0 / 100) * total;
+	}
+	else {
+		hasil = total;
+	}
+
+	return total - hasil;
+}
+
+int main()
+{
+	double bApel, bJeruk, bSemangka;
+	cout << "berat apel : "; cin >> bApel;
+	cout << "berat jeruk : "; cin >> bJeruk;
+	cout << "berat semangka : ";
+	cin >> bSemangka;
+
+	cout << "Total : ";
+	cout << total(bApel, bJeruk, bSemangka);
+}
